66025a6983c3819a3bc646b9ab47d6ed8e041bc0
Christopher Leveston <chrisleveston@gmail.com>
2020-09-18 06:41:14 -0500

    Fixed the links in the Ajax Calls

--- src/js/ajax_calls.js
+++ src/js/ajax_calls.js
@@ -2,7 +2,7 @@
 /*** Loading the New Arrivals Page ***/
     $("#sub-selection-nav p:nth-child(1)").click( () => {
         console.log("Web Icons page was clicked");
-        $("#body-wrapper").load("WebIcons.html", (responseTxt, statusTxt, jqXHR) => {
+        $("#body-wrapper").load("../../WebIcons.html", (responseTxt, statusTxt, jqXHR) => {
             if(statusTxt == "success") {
                 console.log("Web Icons Page loaded successfully");
             }
@@ -15,7 +15,7 @@
     /*** Loading the Apparel Page ***/
     $("#sub-selection-nav p:nth-child(2)").click( () => {
         console.log("Apparel page was clicked.");
-        $("#body-wrapper").load("apparel.html", (responseTxt, statusTxt, jqXHR) => {
+        $("#body-wrapper").load("../../apparel.html", (responseTxt, statusTxt, jqXHR) => {
             if(statusTxt == "success") {
                 console.log("Apparel content loaded successfully.");
             }
@@ -32,7 +32,7 @@
     });
     $("#sub-selection-nav p:nth-child(3)").click( () => {
         console.log("Art page was clicked.");
-        $("#body-wrapper").load("art.html", function(responseTxt, statusTxt, jqXHR){
+        $("#body-wrapper").load("../../art.html", function(responseTxt, statusTxt, jqXHR){
             if(statusTxt == "success") {
                 console.log("Art content loaded successfully.");
             }
@@ -46,7 +46,7 @@
 /*** Loading the Logos Page ***/
     $("#sub-selection-nav p:nth-child(4)").click( () => {
         console.log("Logos page was clicked.");
-        $("#body-wrapper").load("logos.html", (responseTxt, statusTxt, jqXHR) => {
+        $("#body-wrapper").load("../../logos.html", (responseTxt, statusTxt, jqXHR) => {
             if(statusTxt == "success") {
                 console.log("Logos content loaded successfully.");
             }
@@ -59,7 +59,7 @@
     /*** Loading the Album Covers Page ***/
     $("#sub-selection-nav p:nth-child(5)").click( () => {
         console.log("Album cover page was clicked.");
-        $("#body-wrapper").load("albumcover.html", (responseTxt, statusTxt, jqXHR) => {
+        $("#body-wrapper").load("../../albumcover.html", (responseTxt, statusTxt, jqXHR) => {
             if(statusTxt == "success") {
                 console.log("album cover content loaded successfully.");
             }
@@ -72,7 +72,7 @@
     /*** Loading the Illustrations Page ***/
     $("#sub-selection-nav p:nth-child(6)").click( () => {
         console.log("Illustration page was clicked");
-        $("#body-wrapper").load("illustrations.html", (responseTxt, statusTxt, jqXHR) => {
+        $("#body-wrapper").load("../../illustrations.html", (responseTxt, statusTxt, jqXHR) => {
             if(statusTxt == "success") {
                 console.log("illustrations page content loaded successfully.");
             }
